65ec88cc88920b84d6d0bbd8c9304e3dbf42c99e
dingyi <dingyi222666@foxmail.com>
2021-09-23 00:58:22 +0800

    add numpy study code

--- matpoltlib_study.py
+++ matpoltlib_study.py
@@ -8,17 +8,11 @@ def func(out: int):
     return out
 
 
+def figure():
+    pass
+
 def main():
-    fig, ax = plt.subplots()  # 创建一张白纸figure，包含一个坐标系.
-
-    plt.plot(range(-100, 100), [func(index) for index in range(-100, 100)])  # 在坐标系画图.
-    plt.xlabel("x")
-
-    plt.ylabel("y")
-    plt.grid(True)
-    plt.show()
-    # step = (max - min) / (count -1 )
-
+    pass
 
 if __name__ == '__main__':
     main()
---
+++ numpy_study.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+
+def first_array():
+    array = np.array([[1, 2, 3], [3, 4, 5]])  # 两行三列
+    print(array)
+    print(array.reshape([3, 2]))  # 三行两列
+    print(array.ndim)  # 维数
+
+
+def array_2():
+    array = np.arange(0, 20)
+    print(array)
+    print(np.zeros([5, 5]))  # 创建5行5列都为0的数组
+    print(np.ones([3, 4]))  # 创建3行4列都为1的数组
+
+
+def array_3():
+    # 开始值 结束值 生成个数（默认50个)
+    # 计算步长 (最大值-最小值) / （个数 - 1)
+    array = np.linspace(1, 10, 10)
+    print(array)
+    array = np.linspace(1, 10, 10, endpoint=False)
+    # 不包含最后的值
+    print(array, np.linspace(1, 10, 11))  # 其实endpoint就是把取的数字加一，然后舍弃掉最后一个数字
+    print(np.logspace(1, 10, 10, base=2))
+
+
+def array_4():
+    data1 = np.array([[1, 2, 3, 4], [4, 7, 8, 11]])
+
+    data2 = np.array([2, 3, 1, 4])
+
+    '''
+    在进行运算时 因为data1和data2两个的列和维度不一样，data2会先扩展到和data1一样的大小，然后在去和data1里面的元素去做启用
+    '''
+
+    print(data1 + data2)
+
+
+def array_5():
+    data = np.array([1, 3, 4, 5, 6, 7, 8, 7])
+
+    for value in data: #直接for输出
+        print(value)
+
+
+def main():
+    funcs = [first_array, array_2, array_3, array_4,array_5]
+    for func in funcs:
+        print("run function : {} ".format(func.__name__).center(60, "-"))
+        func()
+
+
+if __name__ == '__main__':
+    main()
